25989dad1cb131a98e8c91ffea716ee77b8f95da
venganzaalchocolate <venganzaalchocolate@gmail.com>
2024-04-08 17:37:21 +0200

    cambiando datos

--- renovador.py
+++ renovador.py
@@ -10,7 +10,12 @@ CLIENTE = os.getenv('CLIENTE')
 CLIENTE_CLAVE = os.getenv('CLIENTE_CLAVE')
 RENOVADOR_TOKEN = os.getenv('RENOVADOR_TOKEN')
 
-
+bodyData = {
+    'refresh_token': RENOVADOR_TOKEN,
+    'client_id': CLIENTE,
+    'client_secret': CLIENTE_CLAVE,
+    'grant_type': 'refresh_token'
+}
 
 
 headers = {
