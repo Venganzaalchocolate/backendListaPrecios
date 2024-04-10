9cf6daadcd2ceb67691f109aa2da1d9e3a7a5bc3
venganzaalchocolate <venganzaalchocolate@gmail.com>
2024-04-10 14:11:41 +0200

    añadiendo funcion

--- apizoho.py
+++ apizoho.py
@@ -43,6 +43,37 @@ async def obtenerCasasDisponibles(id):
         else:
           raise HTTPException(status_code=500, detail="Error en el servidor de api de zoho al obtener las casas")
   
+async def camTienePisos(id):
+    try:
+      tokenRenovado= await renovarToken()
+    except:
+      raise HTTPException(status_code=response.status_code, detail="No se pudo renovar el token")
+    
+    headers = {
+    'Authorization': 'Zoho-oauthtoken '+tokenRenovado
+    }
+    
+    urlCampaniasActivas=f"https://www.zohoapis.com/crm/v6/Products/actions/count?criteria=(Status:equals:Available)AND(Promoci_n.id:equals:{id})"
+    
+    try:
+        # Hacer una solicitud GET a la API externa
+        response = requests.get(urlCampaniasActivas, headers=headers )
+        # Verificar el código de estado de la respuesta
+        if response.status_code == 200:
+            # Si la solicitud es exitosa, devolver los datos
+            d=response.json()
+            # return d
+            return d['count']
+        else:
+
+            # Si la solicitud no es exitosa, lanzar una excepción HTTP
+            raise HTTPException(status_code=response.status_code, detail="No se pudo obtener las campañas activas")
+    except Exception as e:
+        # Capturar cualquier error y devolver un mensaje de error genérico
+        if e.status_code==204:
+          raise HTTPException(status_code=204, detail="La campaña no tiene pisos disponibles")
+        else:
+          raise HTTPException(status_code=500, detail="Error en el servidor de api de zoho al obtener las casas")
   
   
   
--- app.py
+++ app.py
@@ -1,8 +1,7 @@
 from fastapi import FastAPI
-from apizoho import obtenerCampaniasActivas, obtenerCasasDisponibles
+from apizoho import obtenerCampaniasActivas, obtenerCasasDisponibles, camTienePisos
 from renovador import renovarToken
 from fastapi.middleware.cors import CORSMiddleware
-
 
 app= FastAPI()
 
@@ -20,8 +19,24 @@ async def token():
 
 @app.get('/api/obtenercampaniasactivas')
 async def obtener_campanias_disponibles():
-    return( await obtenerCampaniasActivas())
+
+    return(await obtenerCampaniasActivas())
 
 @app.get('/api/obtenercasasdisponibles/{idcampania:int}')
 async def obtener_casas_disponibles(idcampania: int):
     return( await obtenerCasasDisponibles(idcampania))
+
+@app.get('/api/camtienepisos/{idcampania:int}')
+async def numero_casas_cam(idcampania: int):
+    return(await camTienePisos(idcampania))
+
+
+    """
+    datos=await obtenerCampaniasActivas()
+    lista=list()
+    for item in datos:
+        idCampania=item['idCampania']
+        tiene=await camTienePisos(idCampania)
+        if tiene['count']>0:
+            lista.append(item)
+    """
