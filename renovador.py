116967b0df146b2cb18d5eef45c7bd3c876c0645
venganzaalchocolate <venganzaalchocolate@gmail.com>
2024-04-19 14:48:26 +0200

    quitando if para probar

--- renovador.py
+++ renovador.py
@@ -28,23 +28,23 @@ async def renovarToken():
     global hora
     global token
 
-    if  hora==0 or datetime.now() > hora + timedelta(minutes=50):
-        try:
+    # if  hora==0 or datetime.now() > hora + timedelta(minutes=50):
+    try:
             # Hacer una solicitud GET a la API externa
             response = requests.post("https://accounts.zoho.com/oauth/v2/token", data=bodyData, headers=headers )
             # Verificar el código de estado de la respuesta
             if response.status_code == 200:
                     # Si la solicitud es exitosa, devolver los datos
                 datos=response.json()
+                print(datos)
                 hora=datetime.now()
                 token=datos['access_token']
                 return token
             else:
                     # Si la solicitud no es exitosa, lanzar una excepción HTTP
                 raise HTTPException(status_code=response.status_code, detail="La solicitud a la API externa falló")
-        except Exception as e:
+    except Exception as e:
                 # Capturar cualquier error y devolver un mensaje de error genérico
-                return e
-                # raise HTTPException(status_code=500, detail="Error al obtener datos de la API externa")
-    else:
-        return token
+                raise HTTPException(status_code=500, detail="Error al obtener datos de la API externa"+e)
+    # else:
+      #  return token
